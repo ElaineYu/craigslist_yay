b0ef409581fd8fb5d8756b37541c5c4e2252e43c
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-29 10:55:29 -0500

    category pages, homepage, db stuff

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,13 @@
 get '/' do
   # Look in app/views/index.erb
+  @categories = Category.all
   erb :index
+end
+
+get '/:cat_name' do
+  @cat = Category.where("name = ?", params[:cat_name])[0].name.capitalize # namestring
+  @cat_id = Category.where("name = ?", params[:cat_name])[0].id # integer
+  @posts = Post.where("category_id = ?", @cat_id).all # all dem posts where...
+
+  erb :cat_posts
 end
---
+++ db/migrate/20131029095640_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.belongs_to :category
+      t.string :title
+      t.string :description
+      t.string :location
+      t.string :price
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
