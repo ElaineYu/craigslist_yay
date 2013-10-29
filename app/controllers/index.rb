3c833e28323c7bd6c357034e159ea66f925e6877
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-29 16:45:41 -0500

    good stuff happened

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,6 +4,8 @@ get '/' do
   erb :index
 end
 
+#SHOW CATEGORY POSTS
+
 get '/:cat_name' do
   @cat = Category.where("name = ?", params[:cat_name]) # category object
   @cat_id = @cat.first.id # integer
@@ -13,12 +15,16 @@ get '/:cat_name' do
   erb :cat_posts
 end
 
+# CREATE A NEW POST
+
 get '/:cat_name/create_post' do
   # display the form so the user can make a new post
      @cat = Category.where("name = ?", params[:cat_name]).first # category object
 
      erb :create_post
 end
+
+#SHOW POST DETAILS
 
 get '/:cat_name/:item_id' do
   @cat = Category.where("name = ?", params[:cat_name]) # category object
@@ -28,6 +34,8 @@ get '/:cat_name/:item_id' do
 
   erb :post_details
 end
+
+#GET USER INFO
 
 post '/:cat_name' do
     @cat = Category.where("name = ?", params[:cat_name]) # category object
@@ -41,4 +49,40 @@ post '/:cat_name' do
 
     redirect to("/#{@cat.first.name}")
 end
+
+#UPDATE
+
+get '/:cat_name/:item_id/edit' do
+  #Find the post
+  @cat = Category.where("name = ?", params[:cat_name]) # category object
+  @cat_id = @cat.first.id # integer
+  @posts = Post.where("category_id = ?", @cat_id) # all dem posts where...
+  @item = Post.find(params[:item_id])
+  
+  erb :edit_post
+end
+
+post '/:cat_name/:item_id/edit' do
+    @item = Post.find(params[:item_id])
+    @cat = Category.where("name = ?", params[:cat_name]) # category object
+    @item.update_attributes(title: params[:title],
+                            description: params[:description],
+                            location: params[:location],
+                            price: params[:price],
+                            email: params[:email])
+    @posts = Post.where("category_id = ?", @cat_id).all # all dem posts where...
+
+    redirect to("/#{@cat.first}")
+end
+
+get '/:cat_name/:item_id/delete' do
+  @item = Post.find(params[:item_id])
+  @cat = Category.where("name = ?", params[:cat_name]) # category object
+  @item.destroy
+
+  redirect to("/#{@cat.first.name}")
+end
+
+
+
 
