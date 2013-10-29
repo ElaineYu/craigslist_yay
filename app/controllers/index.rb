eb29cd39a378f8630527d23499ca10fdf7e5aaba
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-29 15:46:23 -0500

    working, styled

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,9 +5,40 @@ get '/' do
 end
 
 get '/:cat_name' do
-  @cat = Category.where("name = ?", params[:cat_name])[0].name.capitalize # namestring
-  @cat_id = Category.where("name = ?", params[:cat_name])[0].id # integer
+  @cat = Category.where("name = ?", params[:cat_name]) # category object
+  @cat_id = @cat.first.id # integer
+  puts @cat_id
   @posts = Post.where("category_id = ?", @cat_id).all # all dem posts where...
 
   erb :cat_posts
 end
+
+get '/:cat_name/create_post' do
+  # display the form so the user can make a new post
+     @cat = Category.where("name = ?", params[:cat_name]).first # category object
+
+     erb :create_post
+end
+
+get '/:cat_name/:item_id' do
+  @cat = Category.where("name = ?", params[:cat_name]) # category object
+  @cat_id = @cat.first.id # integer
+  @posts = Post.where("category_id = ?", @cat_id) # all dem posts where...
+  @item = Post.find(params[:item_id])
+
+  erb :post_details
+end
+
+post '/:cat_name' do
+    @cat = Category.where("name = ?", params[:cat_name]) # category object
+
+    Category.where("name = ?", @cat.first.name)[0].posts << Post.create(title: params[:title], 
+                                                            description: params[:description], 
+                                                            location: params[:location], 
+                                                            price: params[:price], 
+                                                            email: params[:email])
+    @posts = Post.where("category_id = ?", @cat_id).all # all dem posts where...
+
+    redirect to("/#{@cat.first.name}")
+end
+
